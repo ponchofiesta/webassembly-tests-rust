a3a420ef75a7b550363ae75e66306a417e4277f5
Michael Richter <m.richter@tu-berlin.de>
2019-04-02 16:33:02 +0200

    fibonacci

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,24 @@ extern {
 pub fn greet() {
     alert("Hello, webassembly-tests-rust!");
 }
+
+#[wasm_bindgen]
+pub fn fibonacci(n: i32) -> u64 {
+    if n < 0 {
+        panic!("{} is negative!", n);
+    } else if n == 0 {
+        panic!("zero is not a right argument to fibonacci()!");
+    } else if n == 1 {
+        return 1;
+    }
+
+    let mut sum = 0;
+    let mut last = 0;
+    let mut curr = 1;
+    for _i in 1..n {
+        sum = last + curr;
+        last = curr;
+        curr = sum;
+    }
+    sum
+}
